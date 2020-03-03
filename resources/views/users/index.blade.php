24a499aab01c5c97a1f020f68610f49fc26879e4
Ruslan Banochkin <ruslan@banochkin.com>
2020-03-03 19:50:08 +0300

    Fixed user repeater.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -16,7 +16,7 @@
                 @foreach($users as $user)
                     <tr>
                         <td>{{$user->id}}</td>
-                        <td>{{$user->name}} {{$user->name}}</td>
+                        <td>{{$user->name}}</td>
                         <td>{{$user->email}}</td>
                     </tr>
                 @endforeach
